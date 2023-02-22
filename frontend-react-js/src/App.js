f912efe0da52af672bb058feaa841c746f113481
Elochike <Elochyke@gmail.com>
2023-02-22 12:59:02 +0000

    app.js update

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -10,7 +10,6 @@ import MessageGroupsPage from './pages/MessageGroupsPage';
 import MessageGroupPage from './pages/MessageGroupPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
-import process from 'process';
 import {
   createBrowserRouter,
   RouterProvider
@@ -19,11 +18,11 @@ import {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <NotificationsFeedPage />
+    element: <HomeFeedPage />
   },
   {
-    path: "/",
-    element: <HomeFeedPage />
+    path: "/notifications",
+    element: <NotificationsFeedPage />
   },
   {
     path: "/@:handle",
